4efcc447dc11058d5fe2f4575f675b8807156c0c
FranMejiasCampusCamara <franciscomejias.24@campuscamara.es>
2025-02-13 22:21:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Estás segura??",
+    "Realmente segura??",
+    "De verdad?",
+    "Pookie porfavor...",
+    "Piénsalo por un segundo!",
+    "Si dices que no me pondré muy triste...",
+    "Muy muy triste...",
+    "Pero que muy muy triste...",
+    "Bueno vale, ya no te pregunto más...",
+    "Estaba de broma, di que sí porfavor! ❤️"
 ];
 
 let messageIndex = 0;
